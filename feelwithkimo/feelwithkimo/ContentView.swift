d40ba4a9cf206c4ab443e66823d64de578245100
Ferdinand Lunardy <ferdilunardy@gmail.com>
2025-10-21 12:25:35 +0700

    TGV-16 Feature: Add Breathing Module to Kimo App

--- feelwithkimo/feelwithkimo/ContentView.swift
+++ feelwithkimo/feelwithkimo/ContentView.swift
@@ -9,10 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HomeView()
+        NavigationStack {
+            VStack {
+                HomeView()
+                // Navigation button to BreathingView
+                NavigationLink(destination: BreathingView()) {
+                    HStack {
+                        Text("MizuNoKoyku-JyuIchiNoKata-Nagi")
+                            .font(.title3)
+                            .fontWeight(.medium)
+                    }
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(12)
+                    .padding(.horizontal, 20)
+                }
+                .padding(.bottom, 30)
+            }
         }
-//        .padding()
     }
 }
 
